fed30c8a26f2aacf51d7f17686d4e8dfd5f22737
WangYue <258810561@qq.com>
2016-09-10 19:54:38 +0800

    add 2 line in HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,10 +3,11 @@ public class HelloWorld {
     public static void main(String[] args) {
 
 
-    System.out.println("Hello World!");   
+    System.out.println("Hello World!");
+    System.out.println("Bye");   
     System.out.println("one!");
     System.out.println("two!");
-    	
+    System.out.println("four!");	
 
     }
 
