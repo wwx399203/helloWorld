3416fb598e30063dd09b87a01c57cc33a0fe9edb
wwx399203 <wangyue36@huawei.com>
2016-09-10 19:49:22 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
     System.out.println("Hello World!");   
     System.out.println("one!");
     System.out.println("two!");
-    	
+    System.out.println("three!");
 
     }
 
